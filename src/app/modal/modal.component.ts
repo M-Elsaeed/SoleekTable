f2e73b0470634e8ba5470c8641b7a6466dd982f5
M-Elsaeed <m.elsaeed1998@gmail.com>
2018-10-18 18:33:11 +0200

    Modal component fully Animated, functional and Responsive

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -6,10 +6,35 @@ import { ProductsService } from 'D:/SoleekTable/src/app/products.service';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
-  displayedProduct = this.prodService.getLastProduct();
-  constructor(private prodService: ProductsService) { }
+  displayedProduct = this.prodService.getProductToDisplay();
+  constructor(private prodService: ProductsService) {
+
+
+  }
 
   ngOnInit() {
   }
 
+  /**
+   * This Function allows smooth exit of the Component.
+   * @param htmlStructure This is the main div Container.
+   */
+  destructMe(htmlStructure) {
+    htmlStructure.className = 'main-container';
+
+    this.resetAnimation(htmlStructure);
+
+    setTimeout(() => {
+      this.prodService.shownProduct.showModal = false
+    }, 1000);
+  }
+  /**
+   * Resets the animation of an HTML element
+   * @param element The element whoose animaiton will be reset
+   */
+  resetAnimation(element) {
+    element.style.animation = 'none';
+    element.offsetHeight; /* trigger reflow */
+    element.style.animation = 'fadeOutRight 1s ease 0s 1 normal forwards running';
+  }
 }
