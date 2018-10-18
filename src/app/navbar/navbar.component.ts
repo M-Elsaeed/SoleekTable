5fb7750d70d1bf6559e167d05967dd4f74c817ef
M-Elsaeed <m.elsaeed1998@gmail.com>
2018-10-18 17:56:14 +0200

    NavBar now 100% responsive and customly Animated

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '../../../node_modules/@angular/router';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  constructor(private router: Router) { }
+  // boolean defining if the navbar is open or cloased.
+  active = false;
+  constructor(private router: Router, private location: Location) {
+  }
+  /**
+   * ONLY IF the orientation mandates a side navbar to top navbar transformation, Reload.
+   */
+  reloadComponent() {
+    if (window.innerWidth > 1200 && window.innerHeight <= 1200 || window.innerWidth <= 1200 && window.innerHeight > 1200)
+      location.reload();
+  }
   ngOnInit() {
+    //Registering a listener for orientation change.
+    window.addEventListener("orientationchange", this.reloadComponent, false);
+  }
+  /**
+   * 
+   * @param navBar The navBar element which is binded in the template to #main.
+   * 
+   * This function toggles the animation responsible for hiding and showing of the navbar
+   * as a whole and also of the text content of the navbar.
+   */
+  toggleNav(navBar,content) {
+    console.log(this.active);
+    if (!this.active) {
+      this.active = true;
+      navBar.style.animation = 'none';
+      navBar.offsetHeight; /* trigger reflow */
+      content.style.animation = 'none';
+      content.offsetHeight; /* trigger reflow */
+      content.style.animation = 'fadeIn 1s ease 0s 1 normal forwards running';
+      if (window.innerWidth <= 1200) {
+        navBar.style.animation = 'fadeInDown 1s ease 0s 1 normal forwards running';
+      }
+      else {
+        navBar.style.animation = 'fadeInLeft 1s ease 0s 1 normal forwards running';
+      }
+    }
+    else {
+      this.active = false;
+      navBar.style.animation = 'none';
+      navBar.offsetHeight; /* trigger reflow */
+      content.style.animation = 'none';
+      content.offsetHeight; /* trigger reflow */
+      content.style.animation = 'fadeOut 1s ease 0s 1 normal forwards running';
+      if (window.innerWidth <= 1200) {
+        navBar.style.animation = 'retreatUp-nav 1s ease 0s 1 normal forwards running';
+      }
+      else {
+        navBar.style.animation = 'retreat-nav 1s ease 0s 1 normal forwards running';
+      }
+    }
+
   }
 
 }
